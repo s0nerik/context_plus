a13dec89466fed3517b3e2fa2db709dffcbf1f54
Alex Isaienko <thesonerik@gmail.com>
2023-11-27 22:07:54 +0200

    Return AsyncSnapshot from Stream/ValueStream.watch()

--- lib/src/inherited_stream_context_watch.dart
+++ lib/src/inherited_stream_context_watch.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:collection';
 
 import 'package:flutter/widgets.dart';
 import 'package:meta/meta.dart';
@@ -24,13 +25,41 @@ class InheritedStreamContextWatchElement
     extends ObservableNotifierInheritedElement<Stream, StreamSubscription> {
   InheritedStreamContextWatchElement(super.widget);
 
+  final snapshots = HashMap<StreamSubscription, AsyncSnapshot>();
+
   @override
   StreamSubscription watch(Stream observable, void Function() callback) {
-    return observable.listen((_) => callback());
+    late final StreamSubscription subscription;
+    subscription = observable.listen((data) {
+      snapshots[subscription] =
+          AsyncSnapshot.withData(ConnectionState.active, data);
+      callback();
+    }, onError: (error, trace) {
+      if (trace != null) {
+        snapshots[subscription] =
+            AsyncSnapshot.withError(ConnectionState.active, error, trace);
+      } else {
+        snapshots[subscription] =
+            AsyncSnapshot.withError(ConnectionState.active, error);
+      }
+      callback();
+    }, onDone: () {
+      final data = snapshots[subscription]!.data;
+      if (data != null) {
+        snapshots[subscription] =
+            AsyncSnapshot.withData(ConnectionState.done, data);
+      } else {
+        snapshots[subscription] =
+            const AsyncSnapshot.nothing().inState(ConnectionState.done);
+      }
+      callback();
+    });
+    return subscription;
   }
 
   @override
   void unwatch(Stream observable, StreamSubscription subscription) {
+    snapshots.remove(subscription);
     subscription.cancel();
   }
 }
@@ -43,10 +72,18 @@ extension StreamContextWatchExtension<T> on Stream<T> {
   ///
   /// It is safe to call this method multiple times within the same build
   /// method.
-  void watch(BuildContext context) {
+  AsyncSnapshot<T> watch(BuildContext context) {
     context.dependOnInheritedWidgetOfExactType<InheritedStreamContextWatch>(
       aspect: this,
     );
+
+    final watchRoot = context.getElementForInheritedWidgetOfExactType<
+        InheritedStreamContextWatch>() as InheritedStreamContextWatchElement;
+
+    final subscription =
+        watchRoot.getElementSubscription(context as Element, this);
+    final snapshot = watchRoot.snapshots[subscription];
+    return _convertSnapshot(snapshot);
   }
 }
 
@@ -60,10 +97,58 @@ extension ValueStreamContextWatchExtension<T> on ValueStream<T> {
   ///
   /// It is safe to call this method multiple times within the same build
   /// method.
-  T watch(BuildContext context) {
+  AsyncSnapshot<T> watch(BuildContext context) {
     context.dependOnInheritedWidgetOfExactType<InheritedStreamContextWatch>(
       aspect: this,
     );
-    return value;
+
+    final watchRoot = context.getElementForInheritedWidgetOfExactType<
+        InheritedStreamContextWatch>() as InheritedStreamContextWatchElement;
+
+    final subscription =
+        watchRoot.getElementSubscription(context as Element, this);
+    final snapshot = watchRoot.snapshots[subscription];
+    if (snapshot == null) {
+      if (hasValue) {
+        return AsyncSnapshot<T>.withData(ConnectionState.waiting, value);
+      }
+      if (hasError) {
+        if (stackTrace != null) {
+          return AsyncSnapshot<T>.withError(
+            ConnectionState.waiting,
+            error,
+            stackTrace!,
+          );
+        }
+        return AsyncSnapshot<T>.withError(
+          ConnectionState.waiting,
+          error,
+        );
+      }
+    }
+    return _convertSnapshot(snapshot);
   }
+}
+
+AsyncSnapshot<T> _convertSnapshot<T>(AsyncSnapshot? snapshot) {
+  if (snapshot == null) {
+    return AsyncSnapshot<T>.waiting();
+  }
+  if (snapshot.data != null) {
+    return AsyncSnapshot<T>.withData(snapshot.connectionState, snapshot.data);
+  }
+  if (snapshot.error != null) {
+    if (snapshot.stackTrace != null) {
+      return AsyncSnapshot<T>.withError(
+        snapshot.connectionState,
+        snapshot.error!,
+        snapshot.stackTrace!,
+      );
+    }
+    return AsyncSnapshot<T>.withError(
+      snapshot.connectionState,
+      snapshot.error!,
+    );
+  }
+  return AsyncSnapshot<T>.nothing().inState(snapshot.connectionState);
 }

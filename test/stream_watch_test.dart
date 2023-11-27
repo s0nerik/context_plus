12f7901c76923f0cc49b0be234f4c357a6ae4fff
Alex Isaienko <thesonerik@gmail.com>
2023-11-27 22:04:41 +0200

    stream_watch_test.dart: ValueStream initial value/error tests

--- test/stream_watch_test.dart
+++ test/stream_watch_test.dart
@@ -51,4 +51,66 @@ void main() {
       ]);
     },
   );
+
+  testWidgets(
+    'ValueStream.watch(context) gives AsyncSnapshot.withData right away if it has value',
+    (widgetTester) async {
+      final streamController = BehaviorSubject<int>.seeded(0);
+      final stream = streamController.stream;
+      final snapshots = <AsyncSnapshot<int>>[];
+      final widget = ContextWatchRoot(
+        child: Builder(
+          builder: (context) {
+            final snapshot = stream.watch(context);
+            snapshots.add(snapshot);
+            return const SizedBox.shrink();
+          },
+        ),
+      );
+
+      await widgetTester.pumpWidget(widget);
+      expect(snapshots, [
+        const AsyncSnapshot.withData(ConnectionState.waiting, 0),
+      ]);
+
+      await widgetTester.pumpAndSettle();
+      expect(snapshots, [
+        const AsyncSnapshot.withData(ConnectionState.waiting, 0),
+        const AsyncSnapshot.withData(ConnectionState.active, 0),
+      ]);
+    },
+  );
+
+  testWidgets(
+    'ValueStream.watch(context) gives AsyncSnapshot.withError right away if it has error',
+    (widgetTester) async {
+      final error = Exception('error');
+
+      final streamController = BehaviorSubject<int>();
+      streamController.addError(error);
+
+      final stream = streamController.stream;
+      final snapshots = <AsyncSnapshot<int>>[];
+      final widget = ContextWatchRoot(
+        child: Builder(
+          builder: (context) {
+            final snapshot = stream.watch(context);
+            snapshots.add(snapshot);
+            return const SizedBox.shrink();
+          },
+        ),
+      );
+
+      await widgetTester.pumpWidget(widget);
+      expect(snapshots, [
+        AsyncSnapshot.withError(ConnectionState.waiting, error),
+      ]);
+
+      await widgetTester.pumpAndSettle();
+      expect(snapshots, [
+        AsyncSnapshot.withError(ConnectionState.waiting, error),
+        AsyncSnapshot.withError(ConnectionState.active, error),
+      ]);
+    },
+  );
 }

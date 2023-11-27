d3c63da7a0e711288e18a9f82245106618f2942a
Alex Isaienko <thesonerik@gmail.com>
2023-11-28 00:43:22 +0200

    Created example

---
+++ example/lib/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('context_watch'),
+      ),
+      body: Center(
+        child: Text('Hello World!'),
+      ),
+    );
+  }
+}

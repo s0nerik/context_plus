2d9de060b8da688e8f51a4c53bc93529a03b9077
Alex Isaienko <thesonerik@gmail.com>
2023-11-28 06:00:10 +0200

    example: wip

--- example/lib/home_page.dart
+++ example/lib/home_page.dart
@@ -12,19 +12,24 @@ class HomePage extends StatelessWidget {
       ),
       body: Container(
         alignment: Alignment.center,
-        padding: const EdgeInsets.all(64),
+        padding: const EdgeInsets.all(32),
         child: AspectRatio(
           aspectRatio: 1,
           child: GridView.builder(
             gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 32,
+              crossAxisCount: 20,
             ),
-            itemBuilder: (context, index) => _StreamProvider(
+            itemBuilder: (context, index) => _StreamsProvider(
               key: ValueKey(index),
               initialDelay: Duration(milliseconds: 4 * index),
               delay: const Duration(milliseconds: 48),
-              builder: (context, stream) {
-                return StreamItemBuilder(stream: stream);
+              builder: (context, colorIndexStream, scaleIndexStream) {
+                return StreamItemBuilder(
+                  initialColorIndex: 0,
+                  colorIndexStream: colorIndexStream,
+                  initialScaleIndex: 0,
+                  scaleIndexStream: scaleIndexStream,
+                );
                 // return StreamItem(stream: stream);
               },
             ),
@@ -35,73 +40,112 @@ class HomePage extends StatelessWidget {
   }
 }
 
-class _StreamProvider extends StatefulWidget {
-  const _StreamProvider({
+class _StreamsProvider extends StatefulWidget {
+  const _StreamsProvider({
     super.key,
     required this.builder,
     required this.initialDelay,
     required this.delay,
   });
 
-  final Widget Function(BuildContext context, Stream<int> stream) builder;
+  final Widget Function(
+    BuildContext context,
+    Stream<int> colorIndexStream,
+    Stream<int> scaleIndexStream,
+  ) builder;
   final Duration initialDelay;
   final Duration delay;
 
   @override
-  State<_StreamProvider> createState() => _StreamProviderState();
+  State<_StreamsProvider> createState() => _StreamsProviderState();
 }
 
-class _StreamProviderState extends State<_StreamProvider> {
-  late final stream = Stream.fromFuture(Future.delayed(widget.initialDelay))
-      .asyncExpand((_) => Stream<int>.periodic(widget.delay, (i) => i));
+class _StreamsProviderState extends State<_StreamsProvider> {
+  late final colorIndexStream =
+      Stream.fromFuture(Future.delayed(widget.initialDelay))
+          .asyncExpand((_) => Stream<int>.periodic(widget.delay, (i) => i));
+  late final scaleIndexStream =
+      Stream.fromFuture(Future.delayed(widget.initialDelay))
+          .asyncExpand((_) => Stream<int>.periodic(widget.delay, (i) => i));
 
   @override
   Widget build(BuildContext context) {
-    return widget.builder(context, stream);
+    return widget.builder(context, colorIndexStream, scaleIndexStream);
   }
 }
 
 class StreamItem extends StatelessWidget {
   const StreamItem({
     super.key,
-    required this.stream,
+    required this.colorIndexStream,
+    required this.scaleIndexStream,
   });
 
-  final Stream<int> stream;
+  final Stream<int> colorIndexStream;
+  final Stream<int> scaleIndexStream;
 
   @override
   Widget build(BuildContext context) {
-    return _buildAsyncSnapshot(stream.watch(context));
+    return _build(
+      colorIndexSnapshot: colorIndexStream.watch(context),
+      scaleIndexSnapshot: scaleIndexStream.watch(context),
+    );
   }
 }
 
 class StreamItemBuilder extends StatelessWidget {
   const StreamItemBuilder({
     super.key,
-    this.initialData = 0,
-    required this.stream,
+    required this.initialColorIndex,
+    required this.colorIndexStream,
+    required this.initialScaleIndex,
+    required this.scaleIndexStream,
   });
 
-  final int initialData;
-  final Stream<int> stream;
+  final int initialColorIndex;
+  final Stream<int> colorIndexStream;
+  final int initialScaleIndex;
+  final Stream<int> scaleIndexStream;
 
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
-      initialData: initialData,
-      stream: stream,
-      builder: (context, snapshot) => _buildAsyncSnapshot(snapshot),
+      initialData: initialColorIndex,
+      stream: colorIndexStream,
+      builder: (context, colorIndexSnapshot) => StreamBuilder(
+        initialData: initialScaleIndex,
+        stream: scaleIndexStream,
+        builder: (context, scaleIndexSnapshot) => _build(
+          colorIndexSnapshot: colorIndexSnapshot,
+          scaleIndexSnapshot: scaleIndexSnapshot,
+        ),
+      ),
     );
   }
 }
 
-Widget _buildAsyncSnapshot(AsyncSnapshot<int> colorIndexSnapshot) {
-  return switch (colorIndexSnapshot) {
+Widget _build({
+  required AsyncSnapshot<int> colorIndexSnapshot,
+  required AsyncSnapshot<int> scaleIndexSnapshot,
+}) {
+  final child = switch (colorIndexSnapshot) {
     AsyncSnapshot(hasData: true, requireData: final colorIndex) =>
       ColoredBox(color: _colors[colorIndex % _colors.length]),
     AsyncSnapshot(hasError: false) => const ColoredBox(color: Colors.yellow),
     AsyncSnapshot(hasError: true) => const ColoredBox(color: Colors.red),
   };
+
+  final scaledChild = switch (scaleIndexSnapshot) {
+    AsyncSnapshot(hasData: true, requireData: final scaleIndex) =>
+      Transform.scale(
+        scale: _scales[scaleIndex % _scales.length],
+        child: child,
+      ),
+    AsyncSnapshot(hasError: false) => const ColoredBox(color: Colors.yellow),
+    AsyncSnapshot(hasError: true) => const ColoredBox(color: Colors.red),
+  };
+
+  return scaledChild;
 }
 
 final _colors = _generateGradient(Colors.white, Colors.grey.shade400, 32);
@@ -117,4 +161,19 @@ List<Color> _generateGradient(Color startColor, Color endColor, int steps) {
     gradientColors.add(Color.lerp(endColor, startColor, t)!);
   }
   return gradientColors;
+}
+
+final _scales = _generateScales(0.5, 0.9, 32);
+List<double> _generateScales(double startScale, double endScale, int steps) {
+  List<double> scales = [];
+  int halfSteps = steps ~/ 2; // integer division to get half the steps
+  for (int i = 0; i < halfSteps; i++) {
+    double t = i / (halfSteps - 1);
+    scales.add(startScale + (endScale - startScale) * t);
+  }
+  for (int i = 0; i < halfSteps; i++) {
+    double t = i / (halfSteps - 1);
+    scales.add(endScale + (startScale - endScale) * t);
+  }
+  return scales;
 }

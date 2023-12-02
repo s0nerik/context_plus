f981901b1c38b2a06a824cb0b71f3386b43678bc
Alex Isaienko <thesonerik@gmail.com>
2023-12-02 15:32:28 +0200

    improved stream_watch_benchmark.dart structure

--- example/test/stream_watch_benchmark.dart
+++ example/test/stream_watch_benchmark.dart
@@ -36,8 +36,9 @@ class _Benchmark {
 // Run this with `flutter run --profile test/stream_watch_benchmark.dart`
 main() async {
   assert(false); // fail in debug mode
-  await benchmarkWidgets((WidgetTester tester) async {
-    Future<void> runBenchmark(_Benchmark benchmark) async {
+
+  Future<void> runBenchmark(_Benchmark benchmark) async {
+    await benchmarkWidgets((WidgetTester tester) async {
       await tester.pumpWidget(
         ContextWatchRoot(
           key: UniqueKey(),
@@ -62,106 +63,106 @@ main() async {
           LiveTestWidgetsFlutterBindingFramePolicy.fullyLive;
       await tester.tap(find.byKey(const Key('stop')));
       await tester.pumpAndSettle();
+    });
+  }
+
+  final tileBenchmarks = [1, 10, 100, 1000, 10000, 20000]
+      .expand((tilesCount) => [
+            _Benchmark(
+              dataType: BenchmarkDataType.stream,
+              listenerType: BenchmarkListenerType.contextWatch,
+              singleObservableSubscriptionsCount: 0,
+              tilesCount: tilesCount,
+            ),
+            _Benchmark(
+              dataType: BenchmarkDataType.stream,
+              listenerType: BenchmarkListenerType.streamBuilder,
+              singleObservableSubscriptionsCount: 0,
+              tilesCount: tilesCount,
+            ),
+          ])
+      .toList();
+  final singleObservableBenchmarks = [1, 10, 100, 1000]
+      .expand((singleObservableSubscriptionsCount) => [
+            _Benchmark(
+              dataType: BenchmarkDataType.stream,
+              listenerType: BenchmarkListenerType.contextWatch,
+              singleObservableSubscriptionsCount:
+                  singleObservableSubscriptionsCount,
+              tilesCount: 0,
+            ),
+            _Benchmark(
+              dataType: BenchmarkDataType.stream,
+              listenerType: BenchmarkListenerType.streamBuilder,
+              singleObservableSubscriptionsCount:
+                  singleObservableSubscriptionsCount,
+              tilesCount: 0,
+            ),
+          ])
+      .toList();
+  final benchmarks = [
+    ...tileBenchmarks,
+    ...singleObservableBenchmarks,
+  ];
+
+  for (final benchmark in benchmarks) {
+    await runBenchmark(benchmark);
+  }
+
+  // Stream.watch(context) -> StreamBuilder
+  final comparisons = <_Benchmark, _Benchmark>{};
+  for (int i = 0; i < benchmarks.length; i += 2) {
+    final contextWatchBenchmark = benchmarks[i];
+    final streamBuilderBenchmark = benchmarks[i + 1];
+    comparisons[contextWatchBenchmark] = streamBuilderBenchmark;
+  }
+
+  for (final contextWatchBenchmark in comparisons.keys) {
+    final streamBuilderBenchmark = comparisons[contextWatchBenchmark]!;
+    final contextWatchTime =
+        contextWatchBenchmark.stopwatch.elapsedMicroseconds;
+    final streamBuilderTime =
+        streamBuilderBenchmark.stopwatch.elapsedMicroseconds;
+
+    final fasterName = contextWatchTime <= streamBuilderTime
+        ? 'Stream.watch(context)'
+        : 'StreamBuilder';
+    final fasterTime = contextWatchTime <= streamBuilderTime
+        ? contextWatchTime
+        : streamBuilderTime;
+
+    final slowerName = contextWatchTime > streamBuilderTime
+        ? 'Stream.watch(context)'
+        : 'StreamBuilder';
+    final slowerTime = contextWatchTime > streamBuilderTime
+        ? contextWatchTime
+        : streamBuilderTime;
+
+    final tilesCount = contextWatchBenchmark.benchmark.tilesCount;
+    final observablesPerTile =
+        contextWatchBenchmark.benchmark.observablesPerTile;
+    final singleObservableSubscriptionsCount =
+        contextWatchBenchmark.benchmark.singleObservableSubscriptionsCount;
+    final totalSubscriptionsCount =
+        tilesCount * observablesPerTile + singleObservableSubscriptionsCount;
+
+    final String benchmarkDescription;
+    if (tilesCount == 0 && singleObservableSubscriptionsCount > 0) {
+      benchmarkDescription =
+          '($totalSubscriptionsCount subs total) $singleObservableSubscriptionsCount single stream subscriptions';
+    } else if (tilesCount > 0 && singleObservableSubscriptionsCount == 0) {
+      benchmarkDescription =
+          '($totalSubscriptionsCount subs total) $tilesCount tiles * $observablesPerTile observables';
+    } else {
+      benchmarkDescription =
+          '($totalSubscriptionsCount subs total) $tilesCount tiles * $observablesPerTile observables + $singleObservableSubscriptionsCount global subscriptions';
     }
 
-    final tileBenchmarks = [1, 10, 100, 1000, 10000, 20000]
-        .expand((tilesCount) => [
-              _Benchmark(
-                dataType: BenchmarkDataType.stream,
-                listenerType: BenchmarkListenerType.contextWatch,
-                singleObservableSubscriptionsCount: 0,
-                tilesCount: tilesCount,
-              ),
-              _Benchmark(
-                dataType: BenchmarkDataType.stream,
-                listenerType: BenchmarkListenerType.streamBuilder,
-                singleObservableSubscriptionsCount: 0,
-                tilesCount: tilesCount,
-              ),
-            ])
-        .toList();
-    final singleObservableBenchmarks = [1, 10, 100, 1000]
-        .expand((singleObservableSubscriptionsCount) => [
-              _Benchmark(
-                dataType: BenchmarkDataType.stream,
-                listenerType: BenchmarkListenerType.contextWatch,
-                singleObservableSubscriptionsCount:
-                    singleObservableSubscriptionsCount,
-                tilesCount: 0,
-              ),
-              _Benchmark(
-                dataType: BenchmarkDataType.stream,
-                listenerType: BenchmarkListenerType.streamBuilder,
-                singleObservableSubscriptionsCount:
-                    singleObservableSubscriptionsCount,
-                tilesCount: 0,
-              ),
-            ])
-        .toList();
-    final benchmarks = [
-      ...tileBenchmarks,
-      ...singleObservableBenchmarks,
-    ];
-
-    for (final benchmark in benchmarks) {
-      await runBenchmark(benchmark);
-    }
-
-    // Stream.watch(context) -> StreamBuilder
-    final comparisons = <_Benchmark, _Benchmark>{};
-    for (int i = 0; i < benchmarks.length; i += 2) {
-      final contextWatchBenchmark = benchmarks[i];
-      final streamBuilderBenchmark = benchmarks[i + 1];
-      comparisons[contextWatchBenchmark] = streamBuilderBenchmark;
-    }
-
-    for (final contextWatchBenchmark in comparisons.keys) {
-      final streamBuilderBenchmark = comparisons[contextWatchBenchmark]!;
-      final contextWatchTime =
-          contextWatchBenchmark.stopwatch.elapsedMicroseconds;
-      final streamBuilderTime =
-          streamBuilderBenchmark.stopwatch.elapsedMicroseconds;
-
-      final fasterName = contextWatchTime <= streamBuilderTime
-          ? 'Stream.watch(context)'
-          : 'StreamBuilder';
-      final fasterTime = contextWatchTime <= streamBuilderTime
-          ? contextWatchTime
-          : streamBuilderTime;
-
-      final slowerName = contextWatchTime > streamBuilderTime
-          ? 'Stream.watch(context)'
-          : 'StreamBuilder';
-      final slowerTime = contextWatchTime > streamBuilderTime
-          ? contextWatchTime
-          : streamBuilderTime;
-
-      final tilesCount = contextWatchBenchmark.benchmark.tilesCount;
-      final observablesPerTile =
-          contextWatchBenchmark.benchmark.observablesPerTile;
-      final singleObservableSubscriptionsCount =
-          contextWatchBenchmark.benchmark.singleObservableSubscriptionsCount;
-      final totalSubscriptionsCount =
-          tilesCount * observablesPerTile + singleObservableSubscriptionsCount;
-
-      final String benchmarkDescription;
-      if (tilesCount == 0 && singleObservableSubscriptionsCount > 0) {
-        benchmarkDescription =
-            '($totalSubscriptionsCount subs total) $singleObservableSubscriptionsCount single stream subscriptions';
-      } else if (tilesCount > 0 && singleObservableSubscriptionsCount == 0) {
-        benchmarkDescription =
-            '($totalSubscriptionsCount subs total) $tilesCount tiles * $observablesPerTile observables';
-      } else {
-        benchmarkDescription =
-            '($totalSubscriptionsCount subs total) $tilesCount tiles * $observablesPerTile observables + $singleObservableSubscriptionsCount global subscriptions';
-      }
-
-      final slowerPercent =
-          ((slowerTime / fasterTime - 1) * 100).toStringAsFixed(2);
-      print(
-        '${benchmarkDescription.padRight(60)} | $slowerName[${slowerTime / 1000}ms] is $slowerPercent% slower than $fasterName[${fasterTime / 1000}ms]',
-      );
-    }
-  });
+    final slowerPercent =
+        ((slowerTime / fasterTime - 1) * 100).toStringAsFixed(2);
+    print(
+      '${benchmarkDescription.padRight(60)} | $slowerName[${slowerTime / 1000}ms] is $slowerPercent% slower than $fasterName[${fasterTime / 1000}ms]',
+    );
+  }
   exit(0);
 }

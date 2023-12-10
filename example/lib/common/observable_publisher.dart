cfd69b940c9c9a625923ffda98a1804670618634
Alex Isaienko <thesonerik@gmail.com>
2023-12-10 12:39:08 +0200

    observable_publisher.dart: `count` constructor param

--- example/lib/common/observable_publisher.dart
+++ example/lib/common/observable_publisher.dart
@@ -1,9 +1,10 @@
 import 'dart:async';
+import 'dart:collection';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:rxdart/rxdart.dart';
-import 'package:signals/signals.dart' as signals;
+import 'package:signals/signals.dart' as sgnls;
 
 sealed class ObservablePublisher {
   bool _isDisposed = false;
@@ -24,48 +25,120 @@ sealed class ObservablePublisher {
     }
   }
 
-  @nonVirtual
+  @mustCallSuper
   void dispose() {
     _isDisposed = true;
   }
 }
 
 final class StreamPublisher extends ObservablePublisher {
-  StreamPublisher();
+  StreamPublisher({required int streamCount}) {
+    final streams = <Stream<int>>[];
+    for (var i = 0; i < streamCount; i++) {
+      final streamController = StreamController<int>.broadcast();
+      _streamControllers.add(streamController);
+      streams.add(streamController.stream);
+    }
+    this.streams = UnmodifiableListView(streams);
+  }
 
-  final _streamController = StreamController<int>.broadcast();
-  late final Stream<int> stream = _streamController.stream;
+  final _streamControllers = <StreamController<int>>[];
+  late final List<Stream<int>> streams;
 
   @override
-  void publish(int index) => _streamController.add(index);
+  void publish(int index) {
+    for (final controller in _streamControllers) {
+      controller.add(index);
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    for (final controller in _streamControllers) {
+      controller.close();
+    }
+  }
 }
 
 final class ValueStreamPublisher extends ObservablePublisher {
-  ValueStreamPublisher();
+  ValueStreamPublisher({required int streamCount}) {
+    final streams = <Stream<int>>[];
+    for (var i = 0; i < streamCount; i++) {
+      final subject = BehaviorSubject.seeded(0);
+      _subjects.add(subject);
+      streams.add(subject.stream);
+    }
+    this.streams = UnmodifiableListView(streams);
+  }
 
-  final _subject = BehaviorSubject.seeded(0);
-  late final ValueStream<int> stream = _subject.stream;
+  final _subjects = <BehaviorSubject<int>>[];
+  late final List<Stream<int>> streams;
 
   @override
-  void publish(int index) => _subject.value = index;
+  void publish(int index) {
+    for (final subject in _subjects) {
+      subject.add(index);
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    for (final subject in _subjects) {
+      subject.close();
+    }
+  }
 }
 
 final class ValueNotifierPublisher extends ObservablePublisher {
-  ValueNotifierPublisher();
+  ValueNotifierPublisher({required int notifierCount}) {
+    final valueListenables = <ValueListenable<int>>[];
+    for (var i = 0; i < notifierCount; i++) {
+      final valueNotifier = ValueNotifier<int>(0);
+      _valueNotifiers.add(valueNotifier);
+      valueListenables.add(valueNotifier);
+    }
+    this.valueListenables = UnmodifiableListView(valueListenables);
+  }
 
-  final _valueNotifier = ValueNotifier<int>(0);
-  late final ValueListenable<int> valueListenable = _valueNotifier;
+  final _valueNotifiers = <ValueNotifier<int>>[];
+  late final List<ValueListenable<int>> valueListenables;
 
   @override
-  void publish(int index) => _valueNotifier.value = index;
+  void publish(int index) {
+    for (final notifier in _valueNotifiers) {
+      notifier.value = index;
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    for (final notifier in _valueNotifiers) {
+      notifier.dispose();
+    }
+  }
 }
 
 final class SignalPublisher extends ObservablePublisher {
-  SignalPublisher();
+  SignalPublisher({required int signalCount}) {
+    final signals = <sgnls.Signal<int>>[];
+    for (var i = 0; i < signalCount; i++) {
+      final signal = sgnls.signal(0);
+      _signals.add(signal);
+      signals.add(signal);
+    }
+    this.signals = UnmodifiableListView(signals);
+  }
 
-  final _signal = signals.signal(0);
-  late final signals.Signal<int> signal = _signal;
+  final _signals = <sgnls.Signal<int>>[];
+  late final List<sgnls.ReadonlySignal<int>> signals;
 
   @override
-  void publish(int index) => _signal.value = index;
+  void publish(int index) {
+    for (final signal in _signals) {
+      signal.value = index;
+    }
+  }
 }

c75b4fd176d3770a3b51f9fc1525410e7468629e
Alex Isaienko <thesonerik@gmail.com>
2023-12-31 17:39:27 +0200

    context_ref_watch: Ref.of(context) override

--- packages/context_ref_watch/lib/context_ref_watch.dart
+++ packages/context_ref_watch/lib/context_ref_watch.dart
@@ -1,20 +1,30 @@
-import 'package:context_ref/context_ref.dart';
+import 'package:context_ref/context_ref.dart' as ref;
 import 'package:context_watch/context_watch.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 
-extension RefListenableWatchExt on Ref<Listenable> {
+export 'package:context_ref/context_ref.dart' hide ContextRefExt;
+export 'package:context_watch/context_watch.dart';
+
+extension RefContextWatchExt<T> on ref.Ref<T> {
+  T of(BuildContext context) {
+    context.unwatch();
+    return ref.ContextRefExt(this).of(context);
+  }
+}
+
+extension RefListenableWatchExt on ref.Ref<Listenable> {
   void watch(BuildContext context) => of(context).watch(context);
 }
 
-extension RefValueListenableWatchExt<T> on Ref<ValueListenable<T>> {
+extension RefValueListenableWatchExt<T> on ref.Ref<ValueListenable<T>> {
   T watch(BuildContext context) => of(context).watch(context);
 }
 
-extension RefFutureWatchExt<T> on Ref<Future<T>> {
+extension RefFutureWatchExt<T> on ref.Ref<Future<T>> {
   AsyncSnapshot<T> watch(BuildContext context) => of(context).watch(context);
 }
 
-extension RefStreamWatchExt<T> on Ref<Stream<T>> {
+extension RefStreamWatchExt<T> on ref.Ref<Stream<T>> {
   AsyncSnapshot<T> watch(BuildContext context) => of(context).watch(context);
 }

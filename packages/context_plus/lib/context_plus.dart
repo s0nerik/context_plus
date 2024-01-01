4d96a8f0e9fad32c2d79c0d05c8f1d7d46c61ec3
Alex Isaienko <thesonerik@gmail.com>
2024-01-02 00:24:25 +0200

    migrated context_ref_watch into context_plus

--- packages/context_plus/lib/context_plus.dart
+++ packages/context_plus/lib/context_plus.dart
@@ -1,9 +1,10 @@
-import 'package:context_ref_watch/context_ref_watch.dart';
+import 'package:context_ref/context_ref.dart' as ref;
+import 'package:context_watch/context_watch.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 
-export 'package:context_ref_watch/context_ref_watch.dart'
-    hide ContextRef, ContextWatch;
+export 'package:context_ref/context_ref.dart' hide ContextRefExt;
+export 'package:context_watch/context_watch.dart';
 
 final class ContextPlus {
   ContextPlus._();
@@ -13,7 +14,7 @@ final class ContextPlus {
     List<ContextWatcher> additionalWatchers = const [],
     required Widget child,
   }) {
-    return ContextRef.root(
+    return ref.ContextRef.root(
       child: ContextWatch.root(
         child: child,
       ),
@@ -21,8 +22,31 @@ final class ContextPlus {
   }
 
   static FlutterExceptionHandler? onError(FlutterExceptionHandler? handler) =>
-      ContextRef.onError(handler);
+      ref.ContextRef.onError(handler);
 
   static ErrorWidgetBuilder errorWidgetBuilder(ErrorWidgetBuilder builder) =>
-      ContextRef.errorWidgetBuilder(builder);
+      ref.ContextRef.errorWidgetBuilder(builder);
+}
+
+extension RefContextWatchExt<T> on ref.Ref<T> {
+  T of(BuildContext context) {
+    context.unwatch();
+    return ref.ContextRefExt(this).of(context);
+  }
+}
+
+extension RefListenableWatchExt on ref.Ref<Listenable> {
+  void watch(BuildContext context) => of(context).watch(context);
+}
+
+extension RefValueListenableWatchExt<T> on ref.Ref<ValueListenable<T>> {
+  T watch(BuildContext context) => of(context).watch(context);
+}
+
+extension RefFutureWatchExt<T> on ref.Ref<Future<T>> {
+  AsyncSnapshot<T> watch(BuildContext context) => of(context).watch(context);
+}
+
+extension RefStreamWatchExt<T> on ref.Ref<Stream<T>> {
+  AsyncSnapshot<T> watch(BuildContext context) => of(context).watch(context);
 }

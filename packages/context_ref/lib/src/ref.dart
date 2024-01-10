50c2f22514054ee4844f1bd81fbc87351d5ef233
Alex Isaienko <thesonerik@gmail.com>
2024-01-11 01:39:41 +0200

    ref.dart: some documentation

--- packages/context_ref/lib/src/ref.dart
+++ packages/context_ref/lib/src/ref.dart
@@ -2,11 +2,25 @@ import 'package:flutter/widgets.dart';
 
 import 'context_ref_root.dart';
 
+/// A read-only reference to a value that can be bound to a [BuildContext].
 abstract interface class ReadOnlyRef<T> {}
 
+/// A reference to a value that can be bound to a [BuildContext].
 class Ref<T> implements ReadOnlyRef<T> {
   Ref(); // Must not be const
 
+  /// Bind a value to this [Ref] for this and all descendant [BuildContext]s.
+  ///
+  /// [create] is called only once per [BuildContext] and the value remains
+  /// alive until the [BuildContext] is removed from the tree.
+  ///
+  /// [dispose] is called when the [BuildContext] is removed from the tree.
+  /// If [dispose] is not specified, the provided value will be disposed by
+  /// via `value.dispose()` if such method exists. Use [bindValue] if you don't
+  /// want the value to be disposed automatically.
+  ///
+  /// [key] is used to identify the value. If [key] changes, the old value
+  /// will be disposed and a new value will be created.
   T bind(
     BuildContext context,
     T Function() create, {
@@ -23,6 +37,8 @@ class Ref<T> implements ReadOnlyRef<T> {
     return provider.value;
   }
 
+  /// Same as [bind] but [create] is called lazily, i.e. only when the value
+  /// is requested for the first.
   void bindLazy(
     BuildContext context,
     T Function() create, {
@@ -38,6 +54,15 @@ class Ref<T> implements ReadOnlyRef<T> {
     );
   }
 
+  /// Bind a value to this [Ref] for this and all descendant [BuildContext]s.
+  ///
+  /// [value] is used as the value of this [Ref]. If [value] changes, all
+  /// widgets that depend on this [Ref] will be rebuilt.
+  ///
+  /// This method doesn't manage the lifecycle of the value. It is up to the
+  /// caller to dispose the value when it is no longer needed.
+  ///
+  /// Use [bind] if you want the value to be disposed automatically.
   T bindValue(
     BuildContext context,
     T value,
@@ -50,9 +75,13 @@ class Ref<T> implements ReadOnlyRef<T> {
     return provider.value;
   }
 
+  /// Returns a read-only version of this [Ref]. The returned [ReadOnlyRef]
+  /// can be used to get the value of this [Ref] without being able to change
+  /// the provided value.
   ReadOnlyRef<T> get readOnly => this;
 }
 
 extension ContextRefExt<T> on ReadOnlyRef<T> {
+  /// Get the value of this [Ref] from the given [context].
   T of(BuildContext context) => ContextRefRoot.of(context).get(context, this);
 }

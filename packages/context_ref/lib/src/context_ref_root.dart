47f46fbcc50c37d2701d85b9c20b3ae2251f2298
Alex Isaienko <thesonerik@gmail.com>
2024-01-31 01:31:56 +0200

    context_ref_root.dart refactoring

--- packages/context_ref/lib/src/context_ref_root.dart
+++ packages/context_ref/lib/src/context_ref_root.dart
@@ -38,27 +38,25 @@ class ContextRefRoot extends InheritedWidget {
 class InheritedContextRefElement extends InheritedElement {
   InheritedContextRefElement(super.widget);
 
-  final _contextData = HashMap<BuildContext, _ContextData>.identity();
+  final _elementRefProviders =
+      HashMap<Element, HashMap<ReadOnlyRef, ValueProvider>>.identity();
+  final _elementDependencyRefs =
+      HashMap<Element, HashSet<ReadOnlyRef>>.identity();
   final _refDependentElements =
-      // Ref -> {Element}
       HashMap<ReadOnlyRef, HashSet<Element>>.identity();
 
-  ValueProvider<T>? _getProvider<T>(BuildContext context, ReadOnlyRef<T> ref) {
-    final contextData = _contextData[context];
-    if (contextData == null) {
-      return null;
-    }
-    return contextData.providers[ref] as ValueProvider<T>?;
+  ValueProvider<T>? _getProvider<T>(Element element, ReadOnlyRef<T> ref) =>
+      _elementRefProviders[element]?[ref] as ValueProvider<T>?;
+
+  ValueProvider<T> _getOrCreateProvider<T>(Element element, Ref<T> ref) {
+    final contextProviders =
+        _elementRefProviders[element] ??= HashMap.identity();
+    final provider = contextProviders[ref] ??= ValueProvider<T>();
+    return provider as ValueProvider<T>;
   }
 
-  ValueProvider<T> _getOrCreateProvider<T>(BuildContext context, Ref<T> ref) {
-    final contextData = _contextData[context] ??= _ContextData();
-    return (contextData.providers[ref] ??= ValueProvider<T>())
-        as ValueProvider<T>;
-  }
-
-  HashSet<Element> _getRefDependentElements(ReadOnlyRef ref) =>
-      _refDependentElements[ref] ??= HashSet<Element>.identity();
+  HashSet<Element> _getOrCreateRefDependentElements(ReadOnlyRef ref) =>
+      _refDependentElements[ref] ??= HashSet.identity();
 
   ValueProvider<T> bind<T>({
     required BuildContext context,
@@ -67,11 +65,13 @@ class InheritedContextRefElement extends InheritedElement {
     required void Function(T value)? dispose,
     required Object? key,
   }) {
+    assert(context is Element);
+
     // Make [context] dependent on this element so that we can get notified
     // when the [context] is removed from the tree.
     context.dependOnInheritedElement(this);
 
-    final provider = _getOrCreateProvider(context, ref);
+    final provider = _getOrCreateProvider(context as Element, ref);
     provider.key = key;
     provider.creator = create;
     provider.disposer = dispose;
@@ -83,16 +83,18 @@ class InheritedContextRefElement extends InheritedElement {
     required Ref<T> ref,
     required T value,
   }) {
+    assert(context is Element);
+
     // Make [context] dependent on this element so that we can get notified
     // when the [context] is removed from the tree.
     context.dependOnInheritedElement(this);
 
-    final provider = _getOrCreateProvider(context, ref);
+    final provider = _getOrCreateProvider(context as Element, ref);
     provider.creator = null;
     provider.disposer = _noopDispose;
     if (provider.shouldUpdateValue(value)) {
       provider.value = value;
-      final dependentElements = _getRefDependentElements(ref);
+      final dependentElements = _getOrCreateRefDependentElements(ref);
       for (final element in dependentElements) {
         if (element.mounted) {
           scheduleMicrotask(element.markNeedsBuild);
@@ -109,18 +111,17 @@ class InheritedContextRefElement extends InheritedElement {
     // when the [context] is removed from the tree.
     context.dependOnInheritedElement(this);
 
-    final dependentElements = _getRefDependentElements(ref);
-    dependentElements.add(context as Element);
+    _refDependentElements[ref]?.add(context as Element);
 
-    var provider = _getProvider<T>(context, ref);
+    var provider = _getProvider<T>(context as Element, ref);
     if (provider != null) {
       return provider.value;
     }
 
     context.visitAncestorElements((element) {
-      final p = _contextData[element]?.providers[ref];
+      final p = _getProvider<T>(element, ref);
       if (p != null) {
-        provider = p as ValueProvider<T>;
+        provider = p;
         return false;
       }
       return true;
@@ -141,28 +142,25 @@ class InheritedContextRefElement extends InheritedElement {
   }
 
   void _disposeDependentContextData(BuildContext context) {
-    final contextData = _contextData.remove(context);
-    if (contextData == null) {
-      return;
-    }
-
-    for (final provider in contextData.providers.values) {
+    final refProviders = _elementRefProviders.remove(context);
+    if (refProviders == null) return;
+    for (final provider in refProviders.values) {
       _disposeProvider(provider);
     }
-    for (final ref in contextData.dependencies) {
-      final dependentElements = _getRefDependentElements(ref);
-      dependentElements.remove(context);
+
+    final dependencyRefs = _elementDependencyRefs.remove(context);
+    for (final ref in dependencyRefs ?? const <ReadOnlyRef>[]) {
+      _refDependentElements[ref]?.remove(context);
     }
   }
 
   @override
   void unmount() {
-    for (final contextData in _contextData.values) {
-      for (final provider in contextData.providers.values) {
-        provider.dispose();
+    for (final contextProviders in _elementRefProviders.values) {
+      for (final provider in contextProviders.values) {
+        _disposeProvider(provider);
       }
     }
-    _contextData.clear();
     super.unmount();
   }
 }
@@ -181,11 +179,6 @@ void _disposeProvider<T>(ValueProvider<T> provider) {
       rethrow;
     }
   }
-}
-
-class _ContextData {
-  final providers = HashMap<ReadOnlyRef, ValueProvider>.identity();
-  final dependencies = HashSet<ReadOnlyRef>.identity();
 }
 
 void _noopDispose(_) {}

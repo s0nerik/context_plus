42c6cf4fd886f65fb27c4699f1699449223c1ccf
Alex Isaienko <thesonerik@gmail.com>
2024-02-20 20:39:32 +0200

    counter_example_screen.dart

---
+++ example/lib/examples/counter/context_plus_bind_watch_value_example.dart
@@ -0,0 +1,19 @@
+import 'package:context_plus/context_plus.dart';
+import 'package:flutter/material.dart';
+
+import '../../example.dart';
+
+final _counterNotifier = Ref<ValueNotifier<int>>();
+
+class Example extends StatelessWidget {
+  const Example({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    _counterNotifier.bind(context, () => ValueNotifier(0));
+    return CounterExample(
+      onTap: () => _counterNotifier.of(context).value += 1,
+      counter: _counterNotifier.watchValue(context),
+    );
+  }
+}
---
+++ example/lib/examples/counter_example_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import '../example.dart';
+import 'counter/context_plus_bind_watch_value_example.dart'
+    as context_plus_bind_watch_value;
+
+class CounterExampleScreen extends StatelessWidget {
+  const CounterExampleScreen({super.key});
+
+  static const title = 'Counter';
+  static const urlPath = '/counter_example';
+
+  @override
+  Widget build(BuildContext context) {
+    return const ExampleScaffold(
+      title: title,
+      variants: [
+        ExampleVariant(
+          title: 'context_plus: Ref.bind() + Ref.watchValue()',
+          filePath: 'counter/context_plus_bind_watch_value_example.dart',
+          widget: Center(
+            child: context_plus_bind_watch_value.Example(),
+          ),
+        ),
+      ],
+    );
+  }
+}
